1e6d3aea6f618ed37a4c53383bcfb340beb80356
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2025-06-01 22:46:22 +0000

    test and run

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function isPalindrome(str) {
-    const cleanStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    const reversedStr = cleanStr.split('').reverse().join('');
-    return cleanStr === reversedStr;
+
+
+function fizzBuzz() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+
+    }
 }
 
